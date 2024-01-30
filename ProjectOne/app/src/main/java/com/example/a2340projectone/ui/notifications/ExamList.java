2115ebf4c19b757d5e0523a6b720f1a2eff6d80b
VamsiP23 <90874211+VamsiP23@users.noreply.github.com>
2024-01-30 02:28:12 -0500

    Working List!!!

---
+++ ProjectOne/app/src/main/java/com/example/a2340projectone/ui/notifications/ExamList.java
@@ -0,0 +1,8 @@
+package com.example.a2340projectone.ui.notifications;
+
+import java.util.ArrayList;
+
+public class ExamList {
+    public static ArrayList<Exam> exams = new ArrayList<>();
+
+}
--- ProjectOne/app/src/main/java/com/example/a2340projectone/ui/notifications/NotificationsFragment.java
+++ ProjectOne/app/src/main/java/com/example/a2340projectone/ui/notifications/NotificationsFragment.java
@@ -4,17 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.a2340projectone.R;
 import com.example.a2340projectone.databinding.FragmentNotificationsBinding;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+
+    final ArrayList<Exam> data = new ArrayList<>();
+    List<Exam> items = new ArrayList<>();
+    int counter = 0;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +36,47 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Integer examTitle = null;
+        try {
+            examTitle = NotificationsFragmentArgs.fromBundle(getArguments()).getMyArg();
+        } catch (Exception e) {
+
+        }
+
+//        final TextView textView = binding.textNotifications;
+//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+        items.add(new Exam("FILLERORG", "Whats up", "Hello", "I am", "LOL"));
+
+        Exam examtoBeadded = null;
+        if (examTitle == null) {
+            examtoBeadded = new Exam("FILLER", "Whats up", "Hello", "I am", "LOL");
+            ExamList.exams.add(examtoBeadded);
+        } else {
+            examtoBeadded = new Exam("" + examTitle, "Whats up", "Hello", "I am", "LOL");
+            ExamList.exams.add(examtoBeadded);
+//            items.add(examtoBeadded);
+        }
+
+        RecyclerView recycler = binding.recyclerView;
+        recycler.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        ExamAdapter adapter = new ExamAdapter(ExamList.exams);
+        recycler.setAdapter(adapter);
+        adapter.notifyItemInserted(ExamList.exams.size()-1);
+        binding.addButton.setOnClickListener(view2 -> {
+            NavHostFragment.findNavController(NotificationsFragment.this).navigate(R.id.action_navigation_notifications_to_add_exam);
+
+        });
+
+
+
     }
 
     @Override
